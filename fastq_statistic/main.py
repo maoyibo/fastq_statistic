bb0c543baefac216ff0793ce4cacb38df0e5b8b3
Mao Yibo <maoyibo@gmail.com>
2021-01-07 21:46:26 +0800

    0.1.2 Add options for plot and reserve_data, use multiprocess for calculation.

--- fastq_statistic/main.py
+++ fastq_statistic/main.py
@@ -1,7 +1,8 @@
+import multiprocessing
 from operator import index
 from matplotlib import pyplot as plt
 import subprocess
-from typing import DefaultDict, List, Optional
+from typing import DefaultDict, List, Optional, Tuple
 from io import StringIO
 from pathlib import Path
 import typer
@@ -9,13 +10,16 @@ import pandas as pd
 import numpy as np
 import matplotlib as mpl
 from matplotlib.axes import Axes
+from multiprocessing import Pool
+
+from typer.params import Option
 mpl.use('agg')
 
 
 app = typer.Typer()
 
 
-def run_statistic(filepath: Path) -> pd.DataFrame:
+def run_fastq_statistic(filepath: Path) -> pd.DataFrame:
     script_folder = Path(__file__).absolute().parent
     binary_path = script_folder/"fastq_statistic"
     stat = subprocess.check_output(f"{binary_path} {filepath}", shell=True)
@@ -23,42 +27,63 @@ def run_statistic(filepath: Path) -> pd.DataFrame:
     return df
 
 
-def report(df1: pd.DataFrame, df2: pd.DataFrame, sampleid: str):
+def run_statistic(read1: Path, read2: Optional[Path], reserve=False) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    pool = multiprocessing.Pool(2)
+    read1_thread = pool.apply_async(run_fastq_statistic, (read1,))
+    if read2:
+        read2_thread = pool.apply_async(run_fastq_statistic, (read2,))
+    else:
+        read2_thread = None
+    pool.close()
+    pool.join()
+    read1_df = read1_thread.get()
+    if reserve == True:
+        read1_df.to_csv(read1.with_suffix(".csv"))
+    if read2_thread:
+        read2_df = read2_thread.get()
+        read2_df.to_csv(read2.with_suffix(".csv"))
+    else:
+        read2_df = pd.DataFrame(0, index=read1_df.index,
+                                columns=read1_df.columns)
+    return read1_df, read2_df
+
+
+def report(read1_df: pd.DataFrame, read2_df: pd.DataFrame, sampleid: str):
     df = pd.DataFrame(0, index=[], columns=[])
     df.loc[0, "#SampleID"] = sampleid
-    df.loc[0, "ReadPair"] = df1["Count"].sum()
-    df.loc[0, "Reads"] = df1["Count"].sum()+df2["Count"].sum()
-    df.loc[0, "Bases"] = df1[['A', 'G', 'C', 'T', 'N']
-                             ].sum().sum() + df2[['A', 'G', 'C', 'T', 'N']].sum().sum()
-    df.loc[0, "GCBases"] = df1[['G', 'C']].sum().sum() + \
-        df2[['G', 'C']].sum().sum()
-    df.loc[0, "NBases"] = df1['N'].sum() + df2['N'].sum()
-    df.loc[0, "Q20Bases"] = df1[[x for x in df1.columns[6:] if int(
-        x)-33 >= 20]].sum().sum() + df2[[x for x in df1.columns[6:] if int(x)-33 >= 20]].sum().sum()
-    df.loc[0, "Q30Bases"] = df1[[x for x in df1.columns[6:] if int(
-        x)-33 >= 30]].sum().sum() + df2[[x for x in df1.columns[6:] if int(x)-33 >= 30]].sum().sum()
+    df.loc[0, "ReadPair"] = read1_df["Count"].sum()
+    df.loc[0, "Reads"] = read1_df["Count"].sum()+read2_df["Count"].sum()
+    df.loc[0, "Bases"] = read1_df[['A', 'G', 'C', 'T', 'N']
+                                  ].sum().sum() + read2_df[['A', 'G', 'C', 'T', 'N']].sum().sum()
+    df.loc[0, "GCBases"] = read1_df[['G', 'C']].sum().sum() + \
+        read2_df[['G', 'C']].sum().sum()
+    df.loc[0, "NBases"] = read1_df['N'].sum() + read2_df['N'].sum()
+    df.loc[0, "Q20Bases"] = read1_df[[x for x in read1_df.columns[6:] if int(
+        x)-33 >= 20]].sum().sum() + read2_df[[x for x in read1_df.columns[6:] if int(x)-33 >= 20]].sum().sum()
+    df.loc[0, "Q30Bases"] = read1_df[[x for x in read1_df.columns[6:] if int(
+        x)-33 >= 30]].sum().sum() + read2_df[[x for x in read1_df.columns[6:] if int(x)-33 >= 30]].sum().sum()
     df.loc[0, "GC"] = df.loc[0, "GCBases"]/df.loc[0, "Bases"]
     df.loc[0, "N"] = df.loc[0, "NBases"]/df.loc[0, "Bases"]
     df.loc[0, "Q20"] = df.loc[0, "Q20Bases"]/df.loc[0, "Bases"]
     df.loc[0, "Q30"] = df.loc[0, "Q30Bases"]/df.loc[0, "Bases"]
     read1_length = []
-    for (cycle, count) in df1.loc[df1["Count"] != 0, ["Count"]].iterrows():
+    for (cycle, count) in read1_df.loc[read1_df["Count"] != 0, ["Count"]].iterrows():
         read1_length.extend([cycle]*count["Count"])
     read2_length = []
-    for (cycle, count) in df2.loc[df2["Count"] != 0, ["Count"]].iterrows():
+    for (cycle, count) in read2_df.loc[read2_df["Count"] != 0, ["Count"]].iterrows():
         read2_length.extend([cycle]*count["Count"])
     df.loc[0, "MinLength"] = np.min(read1_length+read2_length)
     df.loc[0, "MeanLength"] = np.mean(read1_length+read2_length)
     df.loc[0, "MedianLength"] = np.median(read1_length+read2_length)
     df.loc[0, "MaxLength"] = np.max(read1_length+read2_length)
     # Read1
-    df.loc[0, "Read1Bases"] = df1[['A', 'G', 'C', 'T', 'N']].sum().sum()
-    df.loc[0, "Read1GCBases"] = df1[['G', 'C']].sum().sum()
-    df.loc[0, "Read1NBases"] = df1['N'].sum()
-    df.loc[0, "Read1Q20Bases"] = df1[[
-        x for x in df1.columns[6:] if int(x)-33 >= 20]].sum().sum()
-    df.loc[0, "Read1Q30Bases"] = df1[[
-        x for x in df1.columns[6:] if int(x)-33 >= 30]].sum().sum()
+    df.loc[0, "Read1Bases"] = read1_df[['A', 'G', 'C', 'T', 'N']].sum().sum()
+    df.loc[0, "Read1GCBases"] = read1_df[['G', 'C']].sum().sum()
+    df.loc[0, "Read1NBases"] = read1_df['N'].sum()
+    df.loc[0, "Read1Q20Bases"] = read1_df[[
+        x for x in read1_df.columns[6:] if int(x)-33 >= 20]].sum().sum()
+    df.loc[0, "Read1Q30Bases"] = read1_df[[
+        x for x in read1_df.columns[6:] if int(x)-33 >= 30]].sum().sum()
     df.loc[0, "Read1GC"] = df.loc[0, "Read1GCBases"]/df.loc[0, "Read1Bases"]
     df.loc[0, "Read1N"] = df.loc[0, "Read1NBases"]/df.loc[0, "Read1Bases"]
     df.loc[0, "Read1Q20"] = df.loc[0, "Read1Q20Bases"]/df.loc[0, "Read1Bases"]
@@ -68,14 +93,15 @@ def report(df1: pd.DataFrame, df2: pd.DataFrame, sampleid: str):
     df.loc[0, "Read1MedianLength"] = np.median(read1_length)
     df.loc[0, "Read1MaxLength"] = np.max(read1_length)
     # Read2
-    if df2["Count"].sum() != 0:
-        df.loc[0, "Read2Bases"] = df2[['A', 'G', 'C', 'T', 'N']].sum().sum()
-        df.loc[0, "Read2GCBases"] = df2[['G', 'C']].sum().sum()
-        df.loc[0, "Read2NBases"] = df2['N'].sum()
-        df.loc[0, "Read2Q20Bases"] = df2[[
-            x for x in df2.columns[6:] if int(x)-33 >= 20]].sum().sum()
-        df.loc[0, "Read2Q30Bases"] = df2[[
-            x for x in df2.columns[6:] if int(x)-33 >= 30]].sum().sum()
+    if read2_df["Count"].sum() != 0:
+        df.loc[0, "Read2Bases"] = read2_df[[
+            'A', 'G', 'C', 'T', 'N']].sum().sum()
+        df.loc[0, "Read2GCBases"] = read2_df[['G', 'C']].sum().sum()
+        df.loc[0, "Read2NBases"] = read2_df['N'].sum()
+        df.loc[0, "Read2Q20Bases"] = read2_df[[
+            x for x in read2_df.columns[6:] if int(x)-33 >= 20]].sum().sum()
+        df.loc[0, "Read2Q30Bases"] = read2_df[[
+            x for x in read2_df.columns[6:] if int(x)-33 >= 30]].sum().sum()
         df.loc[0, "Read2GC"] = df.loc[0, "Read2GCBases"] / \
             df.loc[0, "Read2Bases"] if df.loc[0, "Read2Bases"] != 0 else 0
         df.loc[0, "Read2N"] = df.loc[0, "Read2NBases"] / \
@@ -112,7 +138,7 @@ def collect_plot_data(df: pd.DataFrame):
     return x, data
 
 
-def plot_df(ax: Axes, x: List[int], data: pd.DataFrame, label="Read1"):
+def plot_single_df(ax: Axes, x: List[int], data: pd.DataFrame, label="Read1"):
     ax.bar(x, data["Count"], alpha=0.7,
            align='center', label=f"{label} Read Count")
     for i, l in zip(['A', 'G', 'C', 'T', ], ['-', '--', '-.', ':']):
@@ -129,13 +155,13 @@ def plot_df(ax: Axes, x: List[int], data: pd.DataFrame, label="Read1"):
     ax.set_yticklabels([f'{x}%' for x in range(0, 101, 20)])
 
 
-def plot(df1: pd.DataFrame, df2: pd.DataFrame, filepth: Path):
-    if df2["Count"].sum() != 0:
+def plot_df(read1_df: pd.DataFrame, read2_df: pd.DataFrame, filepth: Path):
+    if read2_df["Count"].sum() != 0:
         fig, (ax1, ax2) = plt.subplots(ncols=2)
-        df1_x, df1_data = collect_plot_data(df1)
-        df2_x, df2_data = collect_plot_data(df2)
-        plot_df(ax1, df1_x, df1_data)
-        plot_df(ax2, df2_x, df2_data, "Read2")
+        read1_x, read1_data = collect_plot_data(read1_df)
+        read2_x, read2_data = collect_plot_data(read2_df)
+        plot_single_df(ax1, read1_x, read1_data)
+        plot_single_df(ax2, read2_x, read2_data, "Read2")
         ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102),
                    loc='lower left', ncol=4, mode="expand", borderaxespad=0.)
         ax2.legend(bbox_to_anchor=(0., 1.02, 1., .102),
@@ -143,8 +169,8 @@ def plot(df1: pd.DataFrame, df2: pd.DataFrame, filepth: Path):
         fig.set_figwidth(fig.get_figwidth()*2.3)
     else:
         fig, ax1 = plt.subplots()
-        df1_x, df1_data = collect_plot_data(df1)
-        plot_df(ax1, df1_x, df1_data)
+        read1_x, read1_data = collect_plot_data(read1_df)
+        plot_single_df(ax1, read1_x, read1_data)
         ax1.legend(bbox_to_anchor=(0., 1.02, 1., .102),
                    loc='lower left', ncol=4, mode="expand", borderaxespad=0.)
         fig.set_figwidth(fig.get_figwidth()*1.2)
@@ -157,20 +183,20 @@ def main(read1: Path = typer.Argument(..., help="Read1 fastq path or fastq path"
              None, help="Read2 filepath or None"),
          sampleid: Optional[str] = typer.Option(
              None, help="SampleID, default is the first item of filename splited by underscore(_)"),
-         result: Optional[Path] = typer.Option(None, help="Result csv file name, plot with use the same name.")):
-    df1 = run_statistic(read1)
-    if read2:
-        df2 = run_statistic(read2)
-    else:
-        df2 = pd.DataFrame(0, index=df1.index, columns=df1.columns)
+         result: Optional[Path] = typer.Option(
+             None, help="Result csv file name, plot with use the same name."),
+         reserve_data: bool = typer.Option(
+             False, help="Reserve fastq statistic intermediate data."),
+         plot: bool = typer.Option(True, help="Plot fastq statistic data.")):
+    read1_df, read2_df = run_statistic(read1, read2, reserve_data)
     # sampleid
     if not sampleid:
         sampleid = read1.stem.split('_')[0]
-    df = report(df1, df2, sampleid)
+    df = report(read1_df, read2_df, sampleid)
     if result:
         df.to_csv(result, index=False)
-        plot(df1, df2, result.with_suffix(".png"))
     else:
         result = Path(f'{sampleid}_statistic.csv')
         df.to_csv(result, index=False)
-        plot(df1, df2, result.with_suffix(".png"))
+    if plot:
+        plot_df(read1_df, read2_df, result.with_suffix(".png"))
